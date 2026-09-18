82d1b93b2edf9cd7860729c593c287669c9fc93d
Hendri <hendri@gmail.com>
2026-09-18 13:15:28 +0700

    Praktikum 1 - proyek Java pertama

---
+++ src/com/pbi/tk/HelloKlienServer.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.pbi.tk;
+
+/**
+ *
+ * @author ASUS
+ */
+public class HelloKlienServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("==============================");
+        System.out.println("Nama  : Hendri Silalahi");
+        System.out.println("NIM   : 25110150");
+        System.out.println("Kelas : TK Grup C");
+        System.out.println("==============================");
+        System.out.println("Aplikasi klien siap dijalankan");
+    }
+
+    
+    
+}
